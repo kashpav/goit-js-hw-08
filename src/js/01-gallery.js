7e27168e3b7e5bd068440fd2811db5f043512b87
Pavlo Kashchenko <pavlo.kashchenko@gmail.com>
2022-08-18 20:53:25 +0200

    it makes 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const render = () => {
+  const galleryItemsMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+
+  console.log(galleryItemsMarkup);
+  galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+};
+
+render();
+
+const newLightBox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+console.log(newLightBox);
