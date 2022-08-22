833abda49d40b86f17fba645009b1d449cbdbb15
Pavlo Kashchenko <pavlo.kashchenko@gmail.com>
2022-08-22 11:05:18 +0200

    add code for 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+var throttle = require(lodash.throttle);
+let savedTime;
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  console.log(localstorage.getItem('videoplayer-current-time'));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+// player.on('timeupdate', onPlay);
+
+savedTime = localStorage.getItem('videoplayer - current - time');
+
+if (savedTime) {
+  player.setCurrentTime(JSON.parse(savedTime));
+}
